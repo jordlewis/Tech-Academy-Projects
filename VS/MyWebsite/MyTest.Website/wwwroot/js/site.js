1ffbf53ef235e82e45c00c1dc0bf2cc55cc99d43
jordlewis <ohgoodjord@gmail.com>
2023-04-30 00:20:23 -0500

    Pushing VS code for review.

---
+++ VS/MyWebsite/MyTest.Website/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+var show_speed = 1000; // every 1000 == 1 second
+var x = setInterval(function () {
+    var texty = document.getElementById('reveal');
+    texty.style.visibility = (texty.style.visibility == 'hidden' ? '' : 'hidden');
+}, show_speed);
